45cf20afa4f2afecc428cff07356b36a7be23086
Nk-Thong <thongk21@gmail.com>
2024-11-15 08:29:17 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -86,7 +86,7 @@ router.get('/getAllUser', function(req, res) {
   //   }
   // ]
   STUDENT.find({}).then(jsonData=>{
-    res.render('users',{name:'Buoi',data:jsonData})
+    res.render('users',{name:'Đã thêm vào sever của thông',data:jsonData})
   })
 });
 router.post('/createUser',function (req,res) {
